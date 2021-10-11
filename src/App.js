f850a0df1be818b3734afbce4e1f66fd3f40054e
jagadhis <jwaran78@gmail.com>
2021-10-11 12:11:08 +0530

    send messages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState(["hello", "hi"]);
+  console.log(input);
+  console.log(messages);
+
+  const sendMessage = (event) => {
+    setMessages([...messages, input]);
+    setInput("");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>lets build a fb-messenger 🚀</h1>
+      <input value={input} onChange={(event) => setInput(event.target.value)} />
+      <button onClick={sendMessage}>Send Message</button>
+      {messages.map((message) => (
+        <p>{message}</p>
+      ))}
     </div>
   );
 }
